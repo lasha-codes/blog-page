3caf6d58d11fdb1c6e3486c7487787d1c16aa076
kaikaci12 <somewonesomewone@gmail.com>
2024-04-03 23:59:30 +0400

    add drop file functionality

--- add blog/script.js
+++ add blog/script.js
@@ -1,12 +1,18 @@
-const dragContainer = document.querySelector(".file-upload");
-const inputFile = document.querySelector("#upload-input");
+const fileInput = document.getElementById("upload-input");
+const dropZone = document.getElementById("dropZone");
 
-dragContainer.addEventListener("dragover", (e) => {
-  console.log("adasd");
-  e.preventDefault();
+dropZone.addEventListener("dragover", (event) => {
+  event.preventDefault();
+  dropZone.classList.add("drag-over");
 });
-dragContainer.addEventListener("drop", (e) => {
-  console.log("adasd");
-  e.preventDefault();
-  inputFile.
+
+dropZone.addEventListener("dragleave", (event) => {
+  dropZone.classList.remove("drag-over");
 });
+dropZone.addEventListener("drop", (event) => {
+  event.preventDefault();
+  dropZone.classList.remove("drag-over");
+  const files = event.dataTransfer.files;
+  fileInput.files = files;
+});
+fileInput.addEventListener("change", () => {});
