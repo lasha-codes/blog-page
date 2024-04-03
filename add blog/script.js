40fdf69bf7e3fd44bf3b9671a346fde07bf795dc
kaikaci12 <somewonesomewone@gmail.com>
2024-04-03 23:25:27 +0400

    add file upload section and start writing functionality

---
+++ add blog/script.js
@@ -0,0 +1,12 @@
+const dragContainer = document.querySelector(".file-upload");
+const inputFile = document.querySelector("#upload-input");
+
+dragContainer.addEventListener("dragover", (e) => {
+  console.log("adasd");
+  e.preventDefault();
+});
+dragContainer.addEventListener("drop", (e) => {
+  console.log("adasd");
+  e.preventDefault();
+  inputFile.
+});
