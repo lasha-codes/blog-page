98543bb9d5053ed19b4293771791d5d23af99ea6
kaikaci12 <somewonesomewone@gmail.com>
2024-04-04 00:49:03 +0400

    add functionalty which checks if image is selected and perfom operation based on it

--- add blog/script.js
+++ add blog/script.js
@@ -1,6 +1,9 @@
 const fileInput = document.getElementById("upload-input");
 const dropZone = document.getElementById("dropZone");
-
+const selectedImg = document.querySelector(".selected-img");
+const imageName = document.getElementById("image-name");
+const closeBtn = document.getElementById("close-btn");
+selectedImg.style.display = "none";
 dropZone.addEventListener("dragover", (event) => {
   event.preventDefault();
   dropZone.classList.add("drag-over");
@@ -14,5 +17,23 @@ dropZone.addEventListener("drop", (event) => {
   dropZone.classList.remove("drag-over");
   const files = event.dataTransfer.files;
   fileInput.files = files;
+  checkSelected();
 });
-fileInput.addEventListener("change", () => {});
+function checkSelected() {
+  if (fileInput.files[0].name) {
+    selectedImg.style.display = "flex";
+    dropZone.style.display = "none";
+    imageName.innerHTML = fileInput.files[0].name;
+  } else {
+    selectedImg.style.display = "none";
+    dropZone.style.display = "flex";
+    imageName.innerHTML = fileInput.files[0].name;
+  }
+}
+fileInput.addEventListener("change", checkSelected);
+closeBtn.addEventListener("click", (e) => {
+  fileInput.value = "";
+  dropZone.style.display = "flex";
+  selectedImg.style.display = "none";
+  imageName.innerHTML = "";
+});
