9c3ead5cbf0b4bc32735fc1dcc3019146e112d32
kaikaci12 <somewonesomewone@gmail.com>
2024-04-13 19:34:28 +0400

    now blogById is an blog object so we use the same renderBlogs function logic to add that blog in the page

--- single-blog.js
+++ single-blog.js
@@ -1,3 +1,5 @@
+const blogSection = document.querySelector(".blogs-section");
+
 const queryParams = new URLSearchParams(window.location.search);
 console.log(queryParams.get("id"));
 
@@ -9,6 +11,65 @@ async function getSingleBlog() {
   const blogById = data.find((blog) => {
     return blog._id === id;
   });
-  console.log(blogById);
+
+  let blogDescr = blogById.description;
+  let blogDate = blogById.date.slice(0, 10);
+  let blogHTML = `
+
+              <div class="user-blog">
+              <img src="${blogById.image}" alt="blog-img" class="blog-img" />
+              <div class="blogger-info">
+              <span class="blogger-name">${blogById.author}</span>
+              <span class="publish-date">${blogDate}</span>
+              <span>${blogById.email}<span>
+              </div>
+              <div class="blog-title">
+                <h2>${blogById.title}</h2>
+                </div>
+                <div class="blog-types-container">
+
+                </div>
+                <div class="blog-description">
+              <p>${blogDescr}</p>
+              </div>
+              </div>
+              `;
+
+  blogSection.innerHTML += blogHTML;
+  let blogContainer = blogSection.lastChild;
+  let typesContainer = document.querySelector(".blog-types-container");
+
+  blogById.types.forEach((type) => {
+    let backgroundColor = "";
+    switch (type) {
+      case "მარკეტი":
+        backgroundColor = "#D6961C";
+        break;
+      case "აპლიკაცია":
+        backgroundColor = "#15C972";
+        break;
+      case "ხელოვნური ინტელექტი":
+        backgroundColor = "#B71FDD";
+        break;
+      case "Figma":
+        backgroundColor = "#08D2AE";
+        break;
+      case "კვლევა":
+        backgroundColor = "#60BE16";
+        break;
+      case "UI/UX":
+        backgroundColor = "#DC2828";
+        break;
+      default:
+        backgroundColor = "#000000";
+    }
+    let button = document.createElement("button");
+    button.classList.add("blog-type-btns");
+    button.textContent = type;
+    button.style.backgroundColor = backgroundColor;
+    button.style.color = "white";
+    typesContainer.appendChild(button);
+  });
 }
+
 getSingleBlog();
