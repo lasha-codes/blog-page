e5f3a9d79e274311174550cf4139cb2ac1725cc2
kaikaci12 <somewonesomewone@gmail.com>
2024-04-13 16:46:49 +0400

    update category blogs filtration functionality

--- app.js
+++ app.js
@@ -12,10 +12,12 @@ const emailInput = document.getElementById("email");
 const emaiLError = document.querySelector(".mail-err-container");
 const shesvla = document.querySelector(".login-container");
 const succeed = document.querySelector(".succeed");
+const seeAll = document.querySelectorAll(".see-all");
 const closeBtn = document.querySelectorAll(".window-close");
 const addBlogBtn = document.getElementById("add-blog-btn");
 const okBtn = document.querySelector(".ok");
 let blogs = document.querySelectorAll(".user-blog");
+
 let blogTypes = [];
 let typeContainers;
 
@@ -101,7 +103,7 @@ function renderBlogs(data) {
         <p>${blogDescr + "..."}</p>
         </div>
         <div class="see-all">
-          <span>სრულად ნახვა</span>
+          <span >სრულად ნახვა</span>
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
           <path d="M5.93415 13.0052C5.64125 13.2981 5.64125 13.773 5.93415 14.0659C6.22704 14.3587 6.70191 14.3587 6.99481 14.0659L5.93415 13.0052ZM14.2855 6.46446C14.2855 6.05024 13.9498 5.71445 13.5355 5.71446L6.78555 5.71445C6.37133 5.71445 6.03555 6.05024 6.03555 6.46445C6.03555 6.87867 6.37133 7.21445 6.78555 7.21445H12.7855V13.2145C12.7855 13.6287 13.1213 13.9645 13.5355 13.9645C13.9498 13.9645 14.2855 13.6287 14.2855 13.2145L14.2855 6.46446ZM6.99481 14.0659L14.0659 6.99478L13.0052 5.93412L5.93415 13.0052L6.99481 14.0659Z" fill="#5D37F3"/>
           </svg>
@@ -197,6 +199,19 @@ function storeBlogAddBtn() {
 }
 addBlogBtn.addEventListener("click", () => {
   console.log("sadasd");
-
   document.location.href = "./add blog/add-blog.html";
 });
+
+seeAll.forEach((button) => {
+  button.addEventListener("click", async () => {
+    console.log(213);
+  });
+});
+async function seeWholeBlog(id) {
+  const response = await fetch(`http://localhost:4000/get-blogs/${id}`);
+  if (!response.ok) {
+    throw new Error("Error fetching data");
+  }
+  const data = await response.json();
+  console.log(data);
+}
